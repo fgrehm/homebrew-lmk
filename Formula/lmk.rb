632bdc5ef372b919dcfc20f5684ffdceeaf2d902
Fabio Rehm <fgrehm@gmail.com>
2019-06-26 15:42:24 -0300

    Update lmk.rb

--- Formula/lmk.rb
+++ Formula/lmk.rb
@@ -1,9 +1,9 @@
 class Lmk < Formula
   desc "A CLI tool to draw your attention to a terminal when a command finishes running"
   homepage "https://github.com/fgrehm/lmk"
-  url "https://github.com/fgrehm/lmk/releases/download/v0.2.0/darwin_amd64"
-  version "1.0.1"
-  sha256 "5ba03a04e5ecc61e806d90580fd86a69af4d4044a1564ac1388073d962257861"
+  url "https://github.com/fgrehm/lmk/releases/download/v1.0.0/darwin_amd64"
+  version "1.1.0"
+  sha256 "1764aa8af6e7f2d7caf487f387fb820765e95b1e72864cf88cbfefde992d5117"
 
   bottle :unneeded
 
